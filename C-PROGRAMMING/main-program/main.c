06d3cb14755d84d24d2aa406cd20530bebd36a53
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-09-04 12:23:48 +0300

    Update main.c

--- C-PROGRAMMING/main-program/main.c
+++ C-PROGRAMMING/main-program/main.c
@@ -23,13 +23,13 @@
 #ifndef AUTOSAR_DATATYPES_H
 #define AUTOSAR_DATATYPES_H
 
-    typedef unsigned char       uint8;     /*           0 .. 255             */
-    typedef unsigned short      uint16;    /*           0 .. 65535           */
-    typedef unsigned long       uint32;    /*           0 .. 4294967295      */
-    typedef unsigned long long  uint64;    /* 0 .. 18446744073709551615      */
-    typedef signed char         sint8;     /*        -128 .. +127            */
-    typedef signed short        sint16;    /*      -32768 .. +32767          */
-    typedef signed long         sint32;    /* -2147483648 .. +2147483647     */
+    typedef unsigned char       uint8;     /*                        0 .. 255             */
+    typedef unsigned short      uint16;    /*                        0 .. 65535           */
+    typedef unsigned long       uint32;    /*                        0 .. 4294967295      */
+    typedef unsigned long long  uint64;    /*              0 .. 18446744073709551615      */
+    typedef signed char         sint8;     /*                     -128 .. +127            */
+    typedef signed short        sint16;    /*                   -32768 .. +32767          */
+    typedef signed long         sint32;    /*              -2147483648 .. +2147483647     */
     typedef signed long long    sint64;    /* -9223372036854775808 .. 9223372036854775807 */
     typedef float               float32;
     typedef double              float64;
